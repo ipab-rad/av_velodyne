eb9ba52c2b93c94b2d71e674b26468a8ddd9d052
hect95 <hector_cruz95@live.com>
2024-05-16 12:27:54 +0100

    Avoid override dev and runtime images

--- dev.sh
+++ dev.sh
@@ -5,11 +5,11 @@
 
 # Build docker image up to dev stage
 DOCKER_BUILDKIT=1 docker build \
-    -t av_velodyne:latest \
+    -t av_velodyne:latest-dev \
     -f Dockerfile --target dev .
 
 # Run docker image with local code volumes for development
 docker run -it --rm --net host --privileged \
     -v /dev/shm:/dev/shm \
     -v ./av_velodyne_launch:/opt/ros_ws/src/av_velodyne_launch \
-    av_velodyne:latest
+    av_velodyne:latest-dev
